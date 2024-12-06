3318c2935c018fefefea826f709689ca67620ce9
Dodie <bonis.dorian@gmail.com>
2024-12-06 06:54:17 +0100

    Server

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from weather import get_current_weather
+#from weather import get_current_weather
 from waitress import serve
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/weather')
+""""@app.route('/weather')
 def get_weather():
     city = request.args.get('city')
 
@@ -33,7 +33,7 @@ def get_weather():
         temp=f"{weather_data['main']['temp']:.1f}",
         feels_like=f"{weather_data['main']['feels_like']:.1f}"
     )
-
+"""
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
